6af513dffad5f2c7eb9f4fb6528de7d119c3506b
coffe <teacoffewater@gmail.com>
2020-02-24 12:52:23 +0000

    for heroku20200224

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,11 @@
 class UsersController < ApplicationController
-before_action :require_user_logged_in, only: [:show]
+before_action :require_user_logged_in
 
   def new
     @user = User.new
   end
   
-  def show
-    @user = User.find(params[:id])
-  end
-  
+
   def create
     @user = User.new(user_params)
 
